44771afb2eeed533d1dd9944e2ae384c31505bc2
valab-rit <bmccis@rit.edu>
2026-01-26 16:27:20 +0000

    Deploying to gh-pages from @ valab-rit/valab-rit.github.io@4083f0d236e89435a7dd3ed576102267ab163ae7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv-contact",
-          title: "cv / contact",
-          description: "",
+        },{id: "nav-cv",
+          title: "CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
