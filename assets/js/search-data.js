1cce33fe2e8b9ebbcce785599fe169dec4b17410
valab-rit <bmccis@rit.edu>
2026-01-26 19:31:04 +0000

    Deploying to gh-pages from @ valab-rit/valab-rit.github.io@e6ff06af077c42a5ba7ea1c7a9181111907a4e92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses",
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-plotly-js",
         
